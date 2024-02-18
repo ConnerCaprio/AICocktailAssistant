4a08498261c1ca39e47133b54d95e24ec4a13b4b
smgpulse007 <smgpulse007@gmail.com>
2024-02-17 22:32:23 -0800

    Update app.py
    Handle exceptions

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ user_preference = st.text_input('Enter your preferred ingredient or cocktail nam
 
 if user_preference:
     recommended_cocktails = recommend_cocktails(user_preference)
-    if recommended_cocktails:
-        st.write('Recommended Cocktails:')
+    if recommended_cocktails and isinstance(recommended_cocktails, list) and recommended_cocktails != []:
+        st.subheader('Recommended Cocktails:')
         # Display each recommended cocktail in a bulleted list
         for cocktail in recommended_cocktails:
             st.markdown(f"- {cocktail}")
+    elif isinstance(recommended_cocktails, str):
+        # Handle the case where a message is returned instead of a list
+        st.write(recommended_cocktails)
     else:
         st.write("No recommendations found. Try another ingredient or cocktail name.")
